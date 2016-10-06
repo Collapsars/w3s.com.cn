73dd6e47552a7c69ccad6d97b866f705d6bc5d3c
503945930 <503945930@qq.com>
2016-10-06 20:17:53 +0800

    m

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -4,6 +4,6 @@ import models from '../models/index';
 
 
 const show = function* (next) {
-    const article = yield models.Article.findById();
+    const article = yield models.Article.findById(3);
     yield this.render('html/html-tutorial');
 }
--- db/migrations/20161005150543-create-articles.js
+++ db/migrations/20161005150543-create-articles.js
@@ -18,6 +18,9 @@ module.exports = {
         title: {
           type: Sequelize.STRING
         },
+        description: {
+          type: Sequelize.STRING
+        },
         content: {
           type: Sequelize.TEXT
         },
